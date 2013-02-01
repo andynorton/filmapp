f17611deeb0344e0b485709068caa7b70bacfba4
Andy Norton <andyjnorton@me.com>
2013-02-01 12:24:10 +0000

    Initial

---
+++ film/Controllers/HomeController.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Net
+Imports System.IO
+
+Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        ViewData("Message") = "Welcome to ASP.NET MVC!"
+
+        Return View()
+    End Function
+
+    Function About() As ActionResult
+        Return View()
+    End Function
+
+    Function SendRequest() As ActionResult
+        Return View()
+
+    End Function
+
+    <HttpPost()> _
+    Function GetResults(searchTerms As String) As JsonResult
+        Dim request = HttpWebRequest.Create("http://www.omdbapi.com/?t=" & searchTerms)
+        Dim serverResponse
+        request.Method = "GET"
+        Using response As HttpWebResponse = TryCast(request.GetResponse(), HttpWebResponse)
+            Using reader As New StreamReader(response.GetResponseStream())
+                serverResponse = reader.ReadToEnd()
+            End Using
+        End Using
+        Return Json(serverResponse)
+    End Function
+
+
+
+
+End Class
---
+++ film/Views/Home/SendRequest.vbhtml
@@ -0,0 +1,42 @@
+﻿@Code
+    ViewData("Title") = "SendRequest"
+End Code
+
+
+<script>
+
+function submitForm() {
+ 
+    var searchTerm = $("input[id$=searchTerms]").val();
+    $.ajax({
+        url: "/home/GetResults",
+        type: "POST",
+        data: { searchTerms: searchTerm },
+        dataType: "json",
+        success: function (data) {
+            var parsedData = JSON.parse(data);
+            alert(data);
+            $('#filmTitle').text(parsedData.Title);
+            $('#filmPlot').text(parsedData.Plot);
+            $('#filmPoster').attr("src", parsedData.Poster);
+
+        },
+        error: function () {
+            alert("error");
+        }
+    });
+}
+</script>
+
+  @Html.TextBox("searchTerms")
+  <input type="button" value="Submit" onclick="submitForm()" />
+
+  <hr />
+  <table>
+
+    <tr>
+        <td><b><span id="filmTitle"></span></b></td><td><img id="filmPoster" width="80px" /></td><td><span id="filmPlot"></span></td>
+    </tr>
+  </table>
+
+
